4a79dea135e22c93ac6e9528d1bc40eacdf307b2
abdulrouf-al <naruto-10-10@hotmail.com>
2022-08-29 00:54:11 +0300

    libs

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+// CommonJS
+ import $ from 'jquery'
+// require('jquery.easing')($);
+
+/*define(['jquery', 'jquery.easing'], function (jQuery, easing) {
+  easing(jQuery)
+})*/
+ 
+import owl from 'owl.carousel'
+import wow from 'wow'
+import waypoints from 'waypoints'
+
+
+// AMD
