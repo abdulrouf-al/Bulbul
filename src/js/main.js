2b26448dad2a5c1ece423e0d7e3a9393327634a4
abdulrouf-al <naruto-10-10@hotmail.com>
2022-08-30 18:57:03 +0300

    index page finished

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,3 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-import i18next from 'i18next';
-
-i18next.init({
-  lng: 'en', // if you're using a language detector, do not define the lng option
-  debug: true,
-  resources: {
-    en: {
-      translation: {
-        "key": "hello world"
-      }
-    }
-  }
-});
