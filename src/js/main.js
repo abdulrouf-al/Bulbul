f420c8e0cdc36ea655fc85c4f477d5aaae72d820
abdulrouf-al <naruto-10-10@hotmail.com>
2022-08-30 13:06:59 +0300

    images, navbar, 1,2 section

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,18 @@
 // Import our custom CSS
 import '../scss/styles.scss'
-
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+import i18next from 'i18next';
 
-// CommonJS
- import $ from 'jquery'
-// require('jquery.easing')($);
-
-/*define(['jquery', 'jquery.easing'], function (jQuery, easing) {
-  easing(jQuery)
-})*/
- 
-import owl from 'owl.carousel'
-import wow from 'wow'
-import waypoints from 'waypoints'
-
-
-// AMD
+i18next.init({
+  lng: 'en', // if you're using a language detector, do not define the lng option
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+        "key": "hello world"
+      }
+    }
+  }
+});
