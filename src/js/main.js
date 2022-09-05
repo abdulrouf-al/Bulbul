44d3d31b5c210f6243c2dd1255ddebf11137f349
abdulrouf-al <naruto-10-10@hotmail.com>
2022-09-06 00:24:23 +0300

    meta

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+
+
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1111);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
