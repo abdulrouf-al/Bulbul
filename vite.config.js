83cd5a3a6c43655dc8012125f8da1fe645edb230
abdulrouf-al <naruto-10-10@hotmail.com>
2022-09-06 23:47:12 +0300

    t1

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve( 'index.html'),
-        course: resolve( 'course.html'),
-        register: resolve( 'register.html')
+        main: resolve( __dirname,'src/index.html'),
+        course: resolve( __dirname,'src/course.html'),
+        register: resolve(__dirname, 'src/register.html')
       }
     }
   }
 })
+
