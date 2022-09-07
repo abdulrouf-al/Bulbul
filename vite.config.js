b105d785225472199d05b628ec05d5e363a3b128
abdulrouf-al <naruto-10-10@hotmail.com>
2022-09-07 17:55:42 +0300

    test 1

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
 
 
-import { resolve } from 'path'
+import { join, resolve } from 'path'
 import { defineConfig } from 'vite'
+import glob from "glob";
 
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve( 'index.html'),
-        course: resolve( 'src/course.html'),
-        register: resolve( 'src/register.html')
-      }
+export default {
+  root: resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      'bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build :{
+  rollupOptions:{
+    outDir:join(__dirname,'dist'),
+    input: {
+      main: resolve( 'src/index.html'),
+      course: resolve( 'src/course.html'),
+      register: resolve( 'src/register.html')
     }
   }
-})
+}
+};
+
+
+
+
 
